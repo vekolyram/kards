83f4bb0d37afc3984267f666dc8b622af2ad922a
vekolyram <vekooolyyyram@outlook.com>
2026-02-01 19:20:45 +0800

    Add files via upload

---
+++ Source/kards/Public/BaseCardObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "BaseCardObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KARDS_API UBaseCardObject : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
